b487669238ef7b5ee8c560f1acf00f186ddfd18e
Roseoketch <oketchrose@gmail.com>
2018-04-23 10:27:08 +0300

    Alchemy file imported

--- app/email.py
+++ app/email.py
@@ -2,8 +2,8 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-subject_pref = 'the C blog'
-sender_email = 'wanjohi.carol75@gmail.com'
+subject_pref = 'My Blog'
+sender_email = 'oketchrose@gmail.com'
 
 def mail_message(subject,template,to,**kwargs):
     email = Message(subject_pref+subject,sender=sender_email,recipients= [user for user in to.split(',')])
